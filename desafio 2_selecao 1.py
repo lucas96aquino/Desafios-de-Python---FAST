46911308664996a75399f189ec030e7b84d55e01
Lucas Aquino <53923879+lucas96aquino@users.noreply.github.com>
2023-10-09 20:58:25 -0300

    Add files via upload

---
+++ desafio 2_selecao 1.py
@@ -0,0 +1,12 @@
+# Usando a biblioteca pandas, faça alguns filtros  no dataset do link anterior
+
+# Selecione as pessoas vacinadas de Recife do sexo feminino da cor parda e preta maior de 60 anos
+# Selecione as pessoas  que se vacinaram nos meses de abril e maio do sexo masculino
+# Além do código, escreva no reade.me como você chegou na solução.
+
+import pandas as pd
+
+df_vacinados = pd.read_csv('vacinados_CERTO CERTO.csv')
+
+df_vacinados_filtrado = df_vacinados.loc[(df_vacinados['sexo'] == 'FEMININO') & ((df_vacinados['raca_cor'] == 'PARDA') | (df_vacinados['raca_cor'] == 'PRETA')) & (df_vacinados['idade'] > 60)]
+print(df_vacinados_filtrado)
