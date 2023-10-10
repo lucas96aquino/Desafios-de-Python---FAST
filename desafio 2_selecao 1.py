f073f1764fd6bacc7b86ec202f9a64e46cc2eac0
Lucas Aquino <53923879+lucas96aquino@users.noreply.github.com>
2023-10-09 22:49:53 -0300

    Add files via upload

--- desafio 2_selecao 1.py
+++ desafio 2_selecao 1.py
@@ -8,5 +8,6 @@ import pandas as pd
 
 df_vacinados = pd.read_csv('vacinados_CERTO CERTO.csv')
 
-df_vacinados_filtrado = df_vacinados.loc[(df_vacinados['sexo'] == 'FEMININO') & ((df_vacinados['raca_cor'] == 'PARDA') | (df_vacinados['raca_cor'] == 'PRETA')) & (df_vacinados['idade'] > 60)]
-print(df_vacinados_filtrado)
+selecao = df_vacinados_filtrado = df_vacinados.loc[(df_vacinados['sexo'] == 'FEMININO') & ((df_vacinados['raca_cor'] == 'PARDA') | (df_vacinados['raca_cor'] == 'PRETA')) & (df_vacinados['idade'] > 60)]
+filtro = selecao[['sexo','raca_cor','idade']]
+print(filtro)
